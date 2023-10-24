bc542997c5833669cb9d95b77686ba5d9ac90f5f
Diego Nascimento <nascimentodln22@gmail.com>
2023-10-24 14:53:11 -0300

    first commit

---
+++ app/src/main/java/com/example/movieapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.utils
+
+object Constants {
+
+    const val API_KEY = "7118e800cceeabe16338daeeea96444b"
+    const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+}
