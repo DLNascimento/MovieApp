238b64e058d7d76081c86d1cc165e407b96aa2ed
Diego Nascimento <nascimentodln22@gmail.com>
2023-11-03 14:41:40 -0300

    Adding Okhttp, Interceptor and Query Parameter

--- app/src/main/java/com/example/movieapp/data/remote/AppModule.kt
+++ app/src/main/java/com/example/movieapp/data/remote/AppModule.kt
@@ -2,13 +2,18 @@ package com.example.movieapp.data.remote
 
 import com.example.movieapp.repository.MovieRepository
 import com.example.movieapp.utils.Constants
+import com.example.movieapp.utils.Constants.API_KEY
+import com.example.movieapp.utils.Constants.BASE_URL
 import com.example.movieapp.viewmodel.GridMoviesViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -18,25 +23,38 @@ object AppModule {
     @Provides
     fun providesBaseUrl() = Constants.BASE_URL
 
+
     @Provides
     @Singleton
-    fun providesRetrofitInstance(BASE_URL: String): ServiceApi =
-        Retrofit.Builder()
+    fun provideMyApi(): ServiceApi {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain ->
+                val originalRequest = chain.request()
+                val url = originalRequest.url.newBuilder()
+                    .addQueryParameter("api_key", API_KEY)
+                    .build()
+                val request = originalRequest.newBuilder().url(url).build()
+                chain.proceed(request)
+            }
+            .build()
+
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ServiceApi::class.java)
-
-
-
-
-    @Provides
-    @Singleton
-    fun providesPokemonListViewModel(repository: MovieRepository): GridMoviesViewModel{
-        return GridMoviesViewModel(repository)
     }
 
 
-
 }
+
 
